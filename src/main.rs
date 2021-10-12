5440dfc12b7e5ff4c663255e1176321aa67ee20f
Alex Samuel <alex@alexsamuel.net>
2021-10-12 16:34:02 -0700

    Initial version.

---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use std::fs::File;
+use std::io::BufRead;
+use std::io::BufReader;
+
+struct Counts {
+    bytes: usize,
+    words: usize,
+    lines: usize,
+}
+
+const EMPTY_COUNTS: Counts = Counts {
+    bytes: 0,
+    words: 0,
+    lines: 0,
+};
+
+fn word_count(string: &str) -> usize {
+    string.split_whitespace().count()
+}
+
+fn main() {
+    let path = match std::env::args().skip(1).next() {
+        Some(p) => p,
+        None => panic!("no file given"),
+    };
+
+    let mut counts = Counts { ..EMPTY_COUNTS };
+
+    let file = File::open(&path).unwrap(); // FIXME
+    let mut reader = BufReader::new(file);
+    let mut line = String::new();
+    loop {
+        line.clear();
+        let read_len = reader.read_line(&mut line).unwrap();
+        if read_len == 0 {
+            // EOF.
+            break;
+        } else {
+            counts.bytes += read_len;
+            counts.words += word_count(&line);
+            counts.lines += 1;
+        }
+    }
+
+    println!(
+        "{} {} {} {}",
+        counts.lines, counts.words, counts.bytes, path
+    );
+}
