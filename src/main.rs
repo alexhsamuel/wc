7a70e77805cbe120dec160134bb628d48ee80eef
Alex Samuel <alex@alexsamuel.net>
2021-10-12 17:57:49 -0700

    Multiple files.

--- src/main.rs
+++ src/main.rs
@@ -27,35 +27,9 @@ fn word_count(string: &str) -> usize {
     string.split_whitespace().count()
 }
 
-fn main() {
-    let exe = std::env::args().next().unwrap();
-    let opts = App::new("word count")
-        .arg(Arg::with_name("bytes").short("c").long("bytes"))
-        .arg(Arg::with_name("chars").short("m").long("chars"))
-        .arg(Arg::with_name("words").short("w").long("words"))
-        .arg(Arg::with_name("lines").short("l").long("lines"))
-        .arg(Arg::with_name("path").value_name("PATH").required(true))
-        .get_matches();
-    let path = opts.value_of("path").unwrap();
-    let show_bytes = opts.is_present("bytes");
-    let mut show_chars = opts.is_present("chars");
-    let mut show_words = opts.is_present("words");
-    let mut show_lines = opts.is_present("lines");
-    if !(show_bytes || show_chars || show_words || show_lines) {
-        show_chars = true;
-        show_words = true;
-        show_lines = true;
-    }
-
+fn count_file(file: &File) -> Counts {
     let mut counts = Counts { ..EMPTY_COUNTS };
 
-    let file = match File::open(&path) {
-        Ok(file) => file,
-        Err(why) => {
-            eprintln!("{}: {}: {}", exe, path, why);
-            std::process::exit(1);
-        }
-    };
     let mut reader = BufReader::new(file);
     let mut line = String::new();
     loop {
@@ -72,17 +46,78 @@ fn main() {
         }
     }
 
+    counts
+}
+
+fn print_counts(path: &str, counts: &Counts, show_lines: bool, show_words: bool, show_chars: bool, show_bytes: bool) {
     if show_lines {
-        print!("{} ", counts.lines);
+        print!("{:8} ", counts.lines);
     }
     if show_words {
-        print!("{} ", counts.words);
+        print!("{:8} ", counts.words);
     }
     if show_chars {
-        print!("{} ", counts.chars);
+        print!("{:8} ", counts.chars);
     }
     if show_bytes {
-        print!("{} ", counts.bytes);
+        print!("{:8} ", counts.bytes);
     }
     println!("{}", path);
+}    
+
+fn main() {
+    let exe = std::env::args().next().unwrap();
+    let opts = App::new("word count")
+        .arg(
+            Arg::with_name("bytes")
+                .short("c")
+                .long("bytes")
+                .help("Prints byte counts"),
+        )
+        .arg(
+            Arg::with_name("chars")
+                .short("m")
+                .long("chars")
+                .help("Prints char counts"),
+        )
+        .arg(
+            Arg::with_name("words")
+                .short("w")
+                .long("words")
+                .help("Prints word counts"),
+        )
+        .arg(
+            Arg::with_name("lines")
+                .short("l")
+                .long("lines")
+                .help("Prints newline counts"),
+        )
+        .arg(
+            Arg::with_name("path")
+                .value_name("PATH")
+                .required(true)
+                .multiple(true),
+        )
+        .get_matches();
+    let show_bytes = opts.is_present("bytes");
+    let mut show_chars = opts.is_present("chars");
+    let mut show_words = opts.is_present("words");
+    let mut show_lines = opts.is_present("lines");
+    if !(show_bytes || show_chars || show_words || show_lines) {
+        show_chars = true;
+        show_words = true;
+        show_lines = true;
+    }
+
+    for path in opts.values_of("path").unwrap() {
+        let file = match File::open(&path) {
+            Ok(file) => file,
+            Err(why) => {
+                eprintln!("{}: {}: {}", exe, path, why);
+                std::process::exit(1);
+            }
+        };
+        let counts = count_file(&file);
+        print_counts(&path, &counts, show_lines, show_words, show_chars, show_bytes);
+    }
 }
